848c0e72ec13e18278c4afaf9cb8352528d1fa01
1293141942@qq.com <1293141942@qq.com>
2021-03-27 21:18:16 +0800

    kotlin coroutine start

---
+++ kotlin-coroutine/src/main/java/com/example/kotlin_coroutine/App.kt
@@ -0,0 +1,19 @@
+package com.example.kotlin_coroutine
+
+import android.app.Application
+import com.example.kotlin_coroutine.exception.GlobalThreadUncaughtExceptionHandler
+import timber.log.Timber
+
+lateinit var appContext: Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+
+        GlobalThreadUncaughtExceptionHandler.setUp()
+
+    }
+}
