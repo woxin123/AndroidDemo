f477ec1f1f2ea1b2e844b8cf5ea255bd37836df3
1293141942@qq.com <1293141942@qq.com>
2021-03-29 17:22:39 +0800

    download flow

--- kotlin-coroutine/src/main/java/com/example/kotlin_coroutine/ui/MainViewModel.kt
+++ kotlin-coroutine/src/main/java/com/example/kotlin_coroutine/ui/MainViewModel.kt
@@ -2,18 +2,32 @@ package com.example.kotlin_coroutine.ui
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.kotlin_coroutine.download.DownloadManager
 import com.example.kotlin_coroutine.download.DownloadManagerRx
 import com.example.kotlin_coroutine.download.DownloadStatus
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class MainViewModel : ViewModel() {
 
     val downloadStatusLiveData = MutableLiveData<DownloadStatus>(DownloadStatus.None)
 
+    private val isUseCoroutine = true
+
     fun download(url: String, fileName: String) {
-        downloadRx(url, fileName)
+        if (!isUseCoroutine) {
+            downloadRx(url, fileName)
+        } else {
+            viewModelScope.launch {
+                downloadSuspend(url, fileName)
+            }
+        }
     }
 
     private fun downloadRx(url: String, fileName: String) {
@@ -22,6 +36,15 @@ class MainViewModel : ViewModel() {
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe {
+                downloadStatusLiveData.value = it
+            }
+    }
+
+    private suspend fun downloadSuspend(url: String, fileName: String) {
+        Timber.d("use flow")
+        DownloadManager.download(url, fileName)
+            .flowOn(Dispatchers.IO)
+            .collect {
                 downloadStatusLiveData.value = it
             }
     }
